1e0be2454c8d137e146c1bae78e6342d2f2f4e22
Diksha <dikshaparab777@gmail.com>
2026-08-11 18:43:34 +0530

    Button component added

---
+++ app/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>UrbanCart</h1>
+            <nav>
+                <span>Home</span>
+                <span>Products</span>
+                <span>Cart</span>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
